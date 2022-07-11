b6be15d6a850dee56314223c397c82349dae9712
CIANDT\hamaraisa <hamaraisa@ciandt.com>
2022-07-11 17:15:54 -0300

    Padrões de projeto Java na prática - Parte 2

---
+++ 0x03/naves/CanhaoExtraDecorator.java
@@ -0,0 +1,11 @@
+package naves;
+
+public class CanhaoExtraDecorator extends NaveEspacialDecorator {
+    public CanhaoExtraDecorator(NaveEspacial naveEspacial) {
+        super(naveEspacial);
+    }
+    @Override
+    public int getAtaque() {
+        return super.getAtaque() + 75;
+    }
+}
---
+++ 0x03/naves/NanoBotsDecorator.java
@@ -0,0 +1,13 @@
+package naves;
+
+public class NanoBotsDecorator extends NaveEspacialDecorator {
+
+    public NanoBotsDecorator(NaveEspacial naveEspacial) {
+        super(naveEspacial);
+    }
+
+    @Override
+    public int getAtaque() {
+        return super.getAtaque() + 25;
+    }
+}
